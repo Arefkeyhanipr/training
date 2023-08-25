13d11b5622070e36a19d7181d08f49ab83018837
Aref <Arefkeyhani.pr@gmail.com>
2023-08-25 21:21:56 +0330

    working with classes

--- app.js
+++ app.js
@@ -1,14 +1,31 @@
-function showAll() {
-  let a = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-  let b = [];
-  let check = a.forEach((item) => {
-    if (item % 2 != 0) {
-      return b.push(item);
-    }
-  });
-  return b;
+// function showAll() {
+//   let a = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+//   let b = [];
+//   let check = a.forEach((item) => {
+//     if (item % 2 != 0) {
+//       return b.push(item);
+//     }
+//   });
+//   return b;
+// }
+
+// let [a, b, ...c] = showAll();
+
+// console.log(a, b, c);
+
+class Enemy {
+  constructor(health, damage, color, name, familyName) {
+    this.health = health;
+    this.damage = damage;
+    this.color = color;
+    this.name = name;
+    this.familyName = familyName;
+  }
+  fullName() {
+    return `its ${this.name} ${this.familyName}`;
+  }
 }
 
-let [a, b, ...c] = showAll();
+let frog = new Enemy(50, 1, "red", "redy", "fro");
 
-console.log(a, b, c);
+console.log(frog.fullName());
