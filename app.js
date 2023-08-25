3631f90482dc86777f86b3ca17504a35f94d236f
Aref <Arefkeyhani.pr@gmail.com>
2023-08-25 20:15:24 +0330

    came up with something

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 function showAll() {
-  return [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  let a = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  let b = [];
+  let check = a.forEach((item) => {
+    if (item % 2 != 0) {
+      return b.push(item);
+    }
+  });
+  return b;
 }
 
-let [a, b, c, d, e, ...f] = showAll();
+let [a, b, ...c] = showAll();
 
-console.log(a, b, c, d, e, f);
+console.log(a, b, c);
